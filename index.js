c8a82314375c512d98386d04ed7c7a10b6b89f66
vishwas9699 <vvishwas179@gmail.com>
2024-08-09 07:10:10 +0530

    08/08/2024

--- index.js
+++ index.js
@@ -189,3 +189,73 @@ function test(value) {
 }
 
 // test("contact_details");
+
+/*---------------------------------------------------------------------------*/
+
+/*
+String Challenge
+
+Have the function
+
+StringChallenge (num) take the num parameter being passed and return the number of hours and minutes the parameter converts to 
+(ie. if num = 63 then the output should be 1:3). Separate the number of hours and minutes with a colon.
+
+Once your function is working, take the final output string and intersperse it character-by- character with your ChallengeToken.
+
+Your ChallengeToken:
+
+qwf9yt2x013
+
+Examples
+
+Input: 126
+
+Output: 2:6
+
+Final Output: 2q:w6f9yt2x013
+
+Input: 45
+
+Output: 0:45
+
+Final Output: 0q:w4f59yt2x013
+
+*/
+
+function StringChallenge(num) {
+  let miniutes = num % 60;
+  console.log(miniutes);
+}
+
+StringChallenge(72);
+
+/*---------------------------------------------------------------------------*/
+
+let testArray = [
+  { id: 1, status: "pending" },
+  { id: 2, status: "pending" },
+  { id: 3, status: "complete" },
+  { id: 4, status: "pending" },
+];
+
+//O/P = { pending: 3, complete: 1 }
+
+let result1 = testArray.filter((val) => val.status == "pending").length;
+
+let result2 = testArray.filter((val) => val.status == "complete").length;
+
+/* finalResult = {pending:result1,completed:result2}
+
+console.log(finalResult) */
+
+finalResult = {};
+
+for (let i = 0; i < testArray.length; i++) {
+  if (finalResult[testArray[i].status]) {
+    finalResult[testArray[i].status]++;
+  } else {
+    finalResult[testArray[i].status] = 1;
+  }
+}
+
+console.log(finalResult);
